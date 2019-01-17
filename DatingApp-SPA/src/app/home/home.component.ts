0845ddfcf82c97d46c3931139b3b3a3d0aa319a6
Muhamad Rifqul Umam <mrifqulumam@outlook.com>
2019-01-17 16:13:08 +0700

    finish section 5

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,16 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
-values: any;
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  values: any = {};
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getValues();
+  }
+
+  registerToogle() {
+    this.registerMode = true;
   }
 
   getValues() {
@@ -23,4 +29,7 @@ values: any;
     });
   }
 
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
 }
